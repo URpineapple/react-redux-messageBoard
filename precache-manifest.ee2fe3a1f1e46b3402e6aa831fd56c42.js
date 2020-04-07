75722d2ba9171b44d8099171ad14bf1cb9d9c40f
URpineapple <zhang.te@husky.neu.edu>
2020-04-07 16:24:54 -0400

    Updates

---
+++ precache-manifest.ee2fe3a1f1e46b3402e6aa831fd56c42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca09098a6e240db2dda2f57735ccf589",
+    "url": "/react-redux-messageBoard/index.html"
+  },
+  {
+    "revision": "9a0f16954b4392933bb7",
+    "url": "/react-redux-messageBoard/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b19837ea145cd8fc133d",
+    "url": "/react-redux-messageBoard/static/css/main.dad6b79e.chunk.css"
+  },
+  {
+    "revision": "9a0f16954b4392933bb7",
+    "url": "/react-redux-messageBoard/static/js/2.ac181c84.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/react-redux-messageBoard/static/js/2.ac181c84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b19837ea145cd8fc133d",
+    "url": "/react-redux-messageBoard/static/js/main.cfbb369f.chunk.js"
+  },
+  {
+    "revision": "0fdd95ec88b1ed3f0a64",
+    "url": "/react-redux-messageBoard/static/js/runtime-main.49e41a76.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-redux-messageBoard/precache-manifest.ee2fe3a1f1e46b3402e6aa831fd56c42.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-redux-messageBoard/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
